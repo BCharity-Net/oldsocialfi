24703deeb0666251b4c17ade430c7db5d51bd41b
BCharity <bcharityfi@gmail.com>
2022-04-09 10:24:37 -0700

    add playground

--- src/pages/daos/index.tsx
+++ src/pages/daos/index.tsx
@@ -19,9 +19,19 @@ function DAO() {
   }, [])
 
   return (
+    // <a className='text-blue-600 px-12 py-12' href='https://playground.skillwallet.id/'> Welcome to play around by clicking me!</a>
     <div className="py-12 mb-4 bg:size-full bg-white border-b bg-hero">
       <div className="px-12 py-12 ml-12">
-        <p className="py-12 text-blue-600">Welcome to your DAO!</p>
+        <a
+          className="py-12 text-blue-600 underline hover:underline-offset-6 visited:text-purple-600 mt-12 pt-12"
+          href="https://playground.skillwallet.id/"
+        >
+          {' '}
+          Please try our Good DAO at the playaround first by clicking me!
+        </a>
+        <p className="py-12 text-purple-600">
+          Cool Degen! Welcome to join Good DAO! Fly to the Moon!
+        </p>
         <sw-auth
           use-dev="true"
           use-button-options="true"
